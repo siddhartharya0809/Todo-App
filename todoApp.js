e01cfdceb1df8b5810897467078b1c075b5b74c9
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2021-03-24 01:19:12 +0530

    HTML
    Updated HTML File

---
+++ todoApp.js
@@ -0,0 +1,10 @@
+// let ulTasks = $('#ulTasks')
+// let btnAdd = $('#btnAdd')
+// let btnClr = $('#btnClr')
+// let inpNewTask = $('#inpNewTask')
+
+// btnAdd.click(() => {
+//     console.log(inpNewTask.val())
+//     inpNewTask.val("")
+// })
+console.log("Hello World")
